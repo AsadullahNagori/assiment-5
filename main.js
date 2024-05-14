1d7ece355b41d00fc69f2abed0d2d69d9def429d
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-14 15:19:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let Auther = "Albert Einstein";
+let code = "A person who never made a mistake never tried anything new.";
+console.log(`${Auther}once said,${code}`);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let Auther:string="Albert Einstein";
+let code:string="A person who never made a mistake never tried anything new.";
+console.log(`${Auther}once said,${code}`);
